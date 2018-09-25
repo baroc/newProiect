e5958ad01348a4319c91a4bf32eabe5d6a7bf0d5
baroc <mybaroc@gmail.comgit>
2018-09-26 00:51:49 +0300

    add  hungry.php

--- hungry.php
+++ hungry.php
@@ -1,3 +1,12 @@
 <?php
-echo "i'm hungry "
-?>
+$t = date("H");
+
+if ($t < "10") {
+    echo "Have a good morning!";
+} elseif ($t < "20") {
+    echo "Have a good day!";
+} else {
+    echo "Have a good night!";
+}
+  ?>
+
